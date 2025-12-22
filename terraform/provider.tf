1d0eb2c144649fe636e1eb64b97527549e6e1f7a
raghu-kadali <raghu2.kadali@gmail.com>
2025-12-22 10:27:40 +0530

    Add Google provider configuration for Terraformp1

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "kubernetes-477004"
+  region  = "us-central1"
+}
